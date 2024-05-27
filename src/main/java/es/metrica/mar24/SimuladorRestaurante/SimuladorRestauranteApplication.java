a90d500cdb37c267f43970d4deb8a9efc5b61ea4
David Sanchez <David Sanchez@P0557>
2024-05-27 12:16:15 +0200

    Initial Commit - Backend

---
+++ src/main/java/es/metrica/mar24/SimuladorRestaurante/SimuladorRestauranteApplication.java
@@ -0,0 +1,13 @@
+package es.metrica.mar24.SimuladorRestaurante;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimuladorRestauranteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimuladorRestauranteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/metrica/mar24/SimuladorRestaurante/SimuladorRestauranteApplicationTests.java
@@ -0,0 +1,13 @@
+package es.metrica.mar24.SimuladorRestaurante;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimuladorRestauranteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
