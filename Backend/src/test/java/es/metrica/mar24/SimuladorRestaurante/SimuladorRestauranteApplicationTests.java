2fa6ac1ae0749944da5a383ca37d84b578a2b398
David Sanchez <145538013+dakotek@users.noreply.github.com>
2024-05-30 14:04:51 +0200

    Test 4
    UserController

--- Backend/src/test/java/es/metrica/mar24/SimuladorRestaurante/SimuladorRestauranteApplicationTests.java
+++ Backend/src/test/java/es/metrica/mar24/SimuladorRestaurante/SimuladorRestauranteApplicationTests.java
@@ -20,6 +20,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import es.metrica.mar24.SimuladorRestaurante.controller.UserController;
 import es.metrica.mar24.SimuladorRestaurante.entities.Rol;
 import es.metrica.mar24.SimuladorRestaurante.entities.User;
 import es.metrica.mar24.SimuladorRestaurante.repositories.UserRepository;
@@ -31,11 +32,14 @@ class SimuladorRestauranteApplicationTests {
 
     private UserService userService;
     private UserRepository userRepository;
+    private UserController userController;
 
     @BeforeEach
     void setUp() {
-        userRepository = mock(UserRepository.class);
+    	userRepository = mock(UserRepository.class);
         userService = new UserService(userRepository);
+        userController = new UserController();
+        userController.userService = userService;
     }
     
 	@Test
@@ -87,7 +91,7 @@ class SimuladorRestauranteApplicationTests {
 	}
 
 	@Test
-    void testGetAllUsers() {
+    void testGetAllUsersService() {
         List<User> userList = new ArrayList<>();
         userList.add(new User(1, "user1", "password1", "user1@example.com", Rol.CLIENT));
         userList.add(new User(2, "user2", "password2", "user2@example.com", Rol.COOK));
@@ -101,46 +105,111 @@ class SimuladorRestauranteApplicationTests {
     }
 	
 	@Test
-    void testGetUserById() {
+    void testGetAllUsersController() {
+        List<User> userList = new ArrayList<>();
+        userList.add(new User(1L, "user1", "password1", "user1@example.com", Rol.CLIENT));
+        userList.add(new User(2L, "user2", "password2", "user2@example.com", Rol.COOK));
+        when(userService.getAllUsers()).thenReturn(userList);
+
+        List<User> result = userController.getAllUsers();
+
+        assertEquals(2, result.size());
+        assertEquals(userList.get(0), result.get(0));
+        assertEquals(userList.get(1), result.get(1));
+    }
+	
+	@Test
+    void testGetUserByIdService() {
         User user = new User(1, "user1", "password1", "user1@example.com", Rol.CLIENT);
         when(userRepository.findById(1L)).thenReturn(Optional.of(user));
 
         Optional<User> result = userService.getUserById(1L);
         assertEquals(user, result.get());
     }
+	
+	@Test
+    void testGetUserByIdController() {
+        User user = new User(1L, "user1", "password1", "user1@example.com", Rol.CLIENT);
+        when(userService.getUserById(1L)).thenReturn(Optional.of(user));
+
+        Optional<User> result = userController.getUserById(1L);
+
+        assertTrue(result.isPresent());
+        assertEquals(user, result.get());
+    }
 
     @Test
-    void testSaveUser() {
+    void testSaveUserService() {
         User user = new User(1, "user1", "password1", "user1@example.com", Rol.CLIENT);
         when(userRepository.save(user)).thenReturn(user);
 
         User savedUser = userService.saveUser(user);
         assertEquals(user, savedUser);
     }
+    
+    @Test
+    void testCreateUserController() {
+        User user = new User(1L, "user1", "password1", "user1@example.com", Rol.CLIENT);
+        when(userService.saveUser(user)).thenReturn(user);
+
+        User result = userController.createUser(user);
+
+        assertEquals(user, result);
+    }
 
     @Test
-    void testDeleteUser() {
+    void testDeleteUserService() {
         userService.deleteUser(1L);
+        verify(userRepository).deleteById(1L);
+    }
+    
+    @Test
+    void testDeleteUserController() {
+    	userController.deleteUser(1L);
         verify(userRepository).deleteById(1L);
     }
 
     @Test
-    void testFindByEmail() {
+    void testFindByEmailService() {
         User user = new User(1, "user1", "password1", "user1@example.com", Rol.CLIENT);
         when(userRepository.findByUsername("user1@example.com")).thenReturn(Optional.of(user));
 
         Optional<User> result = userService.findByEmail("user1@example.com");
         assertEquals(user, result.get());
     }
+    
+    @Test
+    void testGetUserByEmailController() {
+        User user = new User(1L, "user1", "password1", "user1@example.com", Rol.CLIENT);
+        when(userService.findByEmail("user1@example.com")).thenReturn(Optional.of(user));
+
+        Optional<User> result = userController.getUserByEmail("user1@example.com");
+
+        assertTrue(result.isPresent());
+        assertEquals(user, result.get());
+    }
 
     @Test
-    void testUpdateUser() {
+    void testUpdateUserService() {
         User existingUser = new User(1, "user1", "password1", "user1@example.com", Rol.CLIENT);
         User newUser = new User(1, "newUser", "newPassword", "newuser@example.com", Rol.COOK);
         when(userRepository.findById(1L)).thenReturn(Optional.of(existingUser));
         when(userRepository.save(existingUser)).thenReturn(existingUser);
 
         User updatedUser = userService.updateUser(1L, newUser);
+        assertEquals(newUser.getUsername(), updatedUser.getUsername());
+        assertEquals(newUser.getPassword(), updatedUser.getPassword());
+        assertEquals(newUser.getEmail(), updatedUser.getEmail());
+    }
+    
+    @Test
+    void testUpdateUserController() {
+    	User existingUser = new User(1, "user1", "password1", "user1@example.com", Rol.CLIENT);
+        User newUser = new User(1, "newUser", "newPassword", "newuser@example.com", Rol.COOK);
+        when(userRepository.findById(1L)).thenReturn(Optional.of(existingUser));
+        when(userRepository.save(existingUser)).thenReturn(existingUser);
+
+        User updatedUser = userController.updateUser(1L, newUser);
         assertEquals(newUser.getUsername(), updatedUser.getUsername());
         assertEquals(newUser.getPassword(), updatedUser.getPassword());
         assertEquals(newUser.getEmail(), updatedUser.getEmail());
