7695205dc6cd6cb9aaea48b49a335ef6d45b8b29
Adrián <adriantellezblasco@gmail.com>
2024-06-03 11:04:17 +0200

    Arreglado en Login, Vista cliente y Vista cook

---
+++ Frontend/src/app/shared/nav-profile/nav-profile.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-profile',
+  templateUrl: './nav-profile.component.html',
+  styleUrl: './nav-profile.component.css'
+})
+export class NavProfileComponent {
+
+}
