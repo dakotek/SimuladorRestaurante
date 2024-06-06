5cef784623ccf300cd7c8a568aba6599a97effcc
David Sanchez <145538013+dakotek@users.noreply.github.com>
2024-06-06 12:51:16 +0200

    Mostrar pedidos del usuario
    Se muestran todos los pedidos que no estan en estado "CANCELLED" y "COLLECTED"

--- Frontend/src/app/pages/dashboardClient/dashboardClient.component.ts
+++ Frontend/src/app/pages/dashboardClient/dashboardClient.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { JwtHelperService } from '@auth0/angular-jwt';
+import { HttpClient } from '@angular/common/http';
 
 import { infoRecipe } from '../recipe/infoRecipe';
 
@@ -12,8 +13,14 @@ import { infoRecipe } from '../recipe/infoRecipe';
 export class DashboardClientComponent implements OnInit{
 
   token: string | null = null;
+  orders: any[] = [];
 
-  constructor(private router:Router, private jwtHelper: JwtHelperService, private infoRecipe:infoRecipe) {}
+  constructor(
+    private router:Router,
+    private jwtHelper: JwtHelperService,
+    private infoRecipe:infoRecipe,
+    private http: HttpClient
+  ) {}
 
   ngOnInit(): void {
     this.token = localStorage.getItem('token')
@@ -24,10 +31,23 @@ export class DashboardClientComponent implements OnInit{
     if (localStorage.getItem('role') === "COOK") {
       this.router.navigateByUrl('/cocinero');
     }
+  
+    this.getOrders();
   }
 
   goToRecipe(idRecipe: string) {
     localStorage.setItem('recetaSelecc', idRecipe);
     this.router.navigateByUrl('/receta');
+  }
+
+  getOrders(): void {
+    const client = Number(localStorage.getItem('userId'));
+  
+    this.http.get<any[]>('http://localhost:9000/auth/orders')
+      .subscribe(response => {
+        console.log(response);
+        this.orders = response.filter(order => order.status !== 'CANCELLED' && order.status !== 'COLLECTED' && order.client === client);
+        this.orders = this.orders.map(order => `${order.id} - ${order.status}`);
+    });
   }
 }
