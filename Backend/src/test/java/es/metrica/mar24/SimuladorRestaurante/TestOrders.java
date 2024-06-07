4b7a653e9c5e53bc8e6ce8de65d84de867ac55f7
David Sanchez <145538013+dakotek@users.noreply.github.com>
2024-06-07 10:17:10 +0200

    test Orders

--- Backend/src/test/java/es/metrica/mar24/SimuladorRestaurante/TestOrders.java
+++ Backend/src/test/java/es/metrica/mar24/SimuladorRestaurante/TestOrders.java
@@ -1,99 +1,100 @@
 package es.metrica.mar24.SimuladorRestaurante;
 
-
 import org.junit.jupiter.api.Test;
-
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.*;
 import es.metrica.mar24.SimuladorRestaurante.entities.Order;
 import es.metrica.mar24.SimuladorRestaurante.entities.OrderStatus;
-import es.metrica.mar24.SimuladorRestaurante.entities.Rol;
-import es.metrica.mar24.SimuladorRestaurante.entities.User;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestOrders {
 
+    @Mock
+    private Order mockOrder;
+
+    @InjectMocks
+    private Order order;
+
+    public TestOrders() {
+        MockitoAnnotations.openMocks(this);
+    }
+
     @Test
     void testGettersAndSetters() {
-        
-        User client = User.builder()
-                .id(1L)
-                .username("client")
-                .password("password")
-                .email("client@example.com")
-                .role(Rol.CLIENT)
-                .build();
-
-        User cook = User.builder()
-                .id(2L)
-                .username("cook")
-                .password("password")
-                .email("cook@example.com")
-                .role(Rol.COOK)
-                .build();
+        Long clientId = 1L;
+        Long cookId = 2L;
 
         Order order = Order.builder()
-                .client(client)
-                .cook(cook)
+                .client(clientId)
+                .cook(cookId)
                 .status(OrderStatus.PENDING)
-                .recipe("https://www.themealdb.com/api/json/v1/1/lookup.php?i={meal_id}")
+                .recipe("12345")
                 .build();
 
-        assertEquals(client, order.getClient());
-        assertEquals(cook, order.getCook());
+        assertEquals(clientId, order.getClient());
+        assertEquals(cookId, order.getCook());
         assertEquals(OrderStatus.PENDING, order.getStatus());
-        assertEquals("https://www.themealdb.com/api/json/v1/1/lookup.php?i={meal_id}", order.getRecipe());
+        assertEquals("12345", order.getRecipe());
 
-        User newClient = User.builder()
-                .id(3L)
-                .username("newClient")
-                .password("newPassword")
-                .email("newclient@example.com")
-                .role(Rol.CLIENT)
-                .build();
-        order.setClient(newClient);
-
-        User newCook = User.builder()
-                .id(4L)
-                .username("newCook")
-                .password("newPassword")
-                .email("newcook@example.com")
-                .role(Rol.COOK)
-                .build();
-        order.setCook(newCook);
-
+        Long newClientId = 3L;
+        Long newCookId = 4L;
+        order.setClient(newClientId);
+        order.setCook(newCookId);
         order.setStatus(OrderStatus.READY);
         order.setRecipe("newRecipe");
 
-        assertEquals(newClient, order.getClient());
-        assertEquals(newCook, order.getCook());
+        assertEquals(newClientId, order.getClient());
+        assertEquals(newCookId, order.getCook());
         assertEquals(OrderStatus.READY, order.getStatus());
         assertEquals("newRecipe", order.getRecipe());
     }
 
     @Test
     void testConstructor() {
-      
-        User client = User.builder()
-                .id(1L)
-                .username("client")
-                .password("password")
-                .email("client@example.com")
-                .role(Rol.CLIENT)
-                .build();
+        Long clientId = 1L;
+        Long cookId = 2L;
 
-        User cook = User.builder()
-                .id(2L)
-                .username("cook")
-                .password("password")
-                .email("cook@example.com")
-                .role(Rol.COOK)
-                .build();
+        Order order = new Order(clientId, cookId, OrderStatus.PENDING, "12345");
 
-        Order order = new Order(client, cook, OrderStatus.PENDING, "https://www.themealdb.com/api/json/v1/1/lookup.php?i={meal_id}");
-
-        assertEquals(client, order.getClient());
-        assertEquals(cook, order.getCook());
+        assertEquals(clientId, order.getClient());
+        assertEquals(cookId, order.getCook());
         assertEquals(OrderStatus.PENDING, order.getStatus());
-        assertEquals("https://www.themealdb.com/api/json/v1/1/lookup.php?i={meal_id}", order.getRecipe());
+        assertEquals("12345", order.getRecipe());
+    }
+
+    @Test
+    void testBBDD_Mockito() {
+        Long clientId = 1L;
+        Long cookId = 2L;
+        String recipe = "12345";
+        OrderStatus status = OrderStatus.PENDING;
+
+        when(mockOrder.getClient()).thenReturn(clientId);
+        when(mockOrder.getCook()).thenReturn(cookId);
+        when(mockOrder.getRecipe()).thenReturn(recipe);
+        when(mockOrder.getStatus()).thenReturn(status);
+
+        assertEquals(clientId, mockOrder.getClient());
+        assertEquals(cookId, mockOrder.getCook());
+        assertEquals(recipe, mockOrder.getRecipe());
+        assertEquals(status, mockOrder.getStatus());
+
+        Long newClientId = 3L;
+        Long newCookId = 4L;
+        String newRecipe = "newRecipe";
+        OrderStatus newStatus = OrderStatus.READY;
+
+        mockOrder.setClient(newClientId);
+        mockOrder.setCook(newCookId);
+        mockOrder.setRecipe(newRecipe);
+        mockOrder.setStatus(newStatus);
+
+        verify(mockOrder).setClient(newClientId);
+        verify(mockOrder).setCook(newCookId);
+        verify(mockOrder).setRecipe(newRecipe);
+        verify(mockOrder).setStatus(newStatus);
     }
 }
